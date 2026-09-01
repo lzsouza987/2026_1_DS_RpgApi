73be1e20bd7f49fd242aa6d7b9f1829138620936
lzsouza987 <luizfernando987@gmail.com>
2026-09-01 20:06:49 -0300

    Aula 06 - Conexão do Azure para API

--- Program.cs
+++ Program.cs
@@ -9,7 +9,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<DataContext>(options =>
 {
     options.UseSqlServer(builder
-        .Configuration.GetConnectionString("ConexaoLocal"));
+        .Configuration.GetConnectionString("ConexaoAzure"));
 });
 
 // Add services to the container.
--- obj/Debug/net10.0/RpgApi.AssemblyInfo.cs
+++ obj/Debug/net10.0/RpgApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RpgApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5000cb77cbf1f6f942e8ee537fed8b405789c844")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b67685287204caca6dd83cb903ea7ff60b85d9f6")]
 [assembly: System.Reflection.AssemblyProductAttribute("RpgApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RpgApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
